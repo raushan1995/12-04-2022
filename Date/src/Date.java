a9be46795b6d66f6a1314f06e4ac69aef2812e1e
Raushan Kumar <priyanshu1996r@gmail.com>
2023-04-12 22:22:07 +0530

    Add files via upload

---
+++ Date/src/Date.java
@@ -0,0 +1,20 @@
+
+
+
+import java.io.*;
+
+import java.time.*;
+
+class Date {
+
+	// Main driver method
+	public static void main(String[] args)
+	{
+		// Create an object of LoacalDate type
+		LocalDate date = LocalDate.now();
+		// .now() method to tore the current date
+
+		// Print current date
+		System.out.println(date);
+	}
+}
